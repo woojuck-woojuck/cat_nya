55abd8f82d1cc15607e8c03b52519412a5eb93b1
woojuck-woojuck <envymaskjin@gmail.com>
2025-02-07 16:25:11 +0900

    github pages deploy

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-    return <h1>🎉 React App</h1>;
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
